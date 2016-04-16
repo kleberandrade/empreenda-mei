4f805e80072ed39683d4450a286d4d4565b22291
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2016-04-15 21:16:13 -0300

    Script de PauseManager criado

---
+++ Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject m_PausePanel;
+    private AudioSource m_AudioSource;
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+    
+	private void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Change();
+        }
+	}
+
+    public void Change()
+    {
+        m_AudioSource.Play();
+        m_PausePanel.SetActive(!m_PausePanel.activeInHierarchy);
+        Time.timeScale = 1 - Time.timeScale;
+    }
+
+    public void QuitGame()
+    {
+        Change();
+        SceneManager.LoadScene("Menu");
+    }
+}
